7928e2e0177eb180b859d8ddce7455695972f2fa
Orintaa <orinta.strac@gmail.com>
2022-03-09 23:10:53 +0200

    finished modal

--- starter/script.js
+++ starter/script.js
@@ -1,4 +1,22 @@
 'use strict';
 
-const modal = document.querySelector('.model');
+const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+const modalHidden = () => {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', () => {
+    console.log('Button clicked');
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+
+  btnCloseModal.addEventListener('click', modalHidden);
+  overlay.addEventListener('click', modalHidden);
+}
